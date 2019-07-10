1590b504163eff6c191f771bae2fb4c5daba129d
elenoko <elenoko@gmail.com>
2019-07-10 14:44:24 +0100

    Added UserDetailSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,18 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
 class UserSerializer(serializers.ModelSerializer):
+    """ Users list serializer"""
     class Meta:
         model = get_user_model()
         fields = ('id', 'email')
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    """ User Detail serilalizer """
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'id', 'email',
+            'first_name', 'last_name',
+            'last_login', 'date_joined')
+        lookup_field = 'email'
--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from .views import UserList
+from .views import UserList, UserDetails
 
 urlpatterns = [
 
-
     path('users/', UserList.as_view()),
+    path('users/<str:email>/', UserDetails.as_view()),
 ]
