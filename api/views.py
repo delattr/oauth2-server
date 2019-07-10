e44893f81db93db1eb642b8e45dbcb8337843924
elenoko <elenoko@gmail.com>
2019-07-10 14:43:33 +0100

    Add UserDetailView

--- api/views.py
+++ api/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
-from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope
 from django.contrib.auth import get_user_model
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserDetailSerializer
 
 CustomUser = get_user_model()
 
 class UserList(generics.ListCreateAPIView):
+    """ List all users"""
+
     permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+
+class UserDetails(generics.RetrieveAPIView):
+    """ User detail view.  returns email, first name and last name"""
+
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    queryset = CustomUser.objects.all()
+    serializer_class = UserDetailSerializer
+    lookup_field = 'email'
